c62600e10b75621b3d3c2c9e56dca12b66195688
kriegerj <kriegerj@0d8be23c-4016-4ca5-a69c-3788035ecc6c>
2011-07-05 14:43:17 +0000

    wip
    git-svn-id: https://www.dkfz.de/svn/B040/FCSTOOLS/trunk/QuickFit3@773 0d8be23c-4016-4ca5-a69c-3788035ecc6c

--- application/version.h
+++ application/version.h
@@ -4,10 +4,10 @@
 namespace AutoVersion{
 	
 	//Date Version Types
-	static const char DATE[] = "29";
-	static const char MONTH[] = "06";
+	static const char DATE[] = "05";
+	static const char MONTH[] = "07";
 	static const char YEAR[] = "2011";
-	static const char UBUNTU_VERSION_STYLE[] = "11.06";
+	static const char UBUNTU_VERSION_STYLE[] = "11.07";
 	
 	//Software Status
 	static const char STATUS[] = "Beta";
@@ -16,14 +16,14 @@ namespace AutoVersion{
 	//Standard Version Type
 	static const long MAJOR = 3;
 	static const long MINOR = 0;
-	static const long BUILD = 164;
-	static const long REVISION = 6;
+	static const long BUILD = 165;
+	static const long REVISION = 7;
 	
 	//Miscellaneous Version Types
-	static const long BUILDS_COUNT = 446;
-	#define RC_FILEVERSION 3,0,164,6
-	#define RC_FILEVERSION_STRING "3, 0, 164, 6\0"
-	static const char FULLVERSION_STRING[] = "3.0.164.6";
+	static const long BUILDS_COUNT = 474;
+	#define RC_FILEVERSION 3,0,165,7
+	#define RC_FILEVERSION_STRING "3, 0, 165, 7\0"
+	static const char FULLVERSION_STRING[] = "3.0.165.7";
 	
 	//These values are to keep track of your versioning state, don't modify them.
 	static const long BUILD_HISTORY = 0;
