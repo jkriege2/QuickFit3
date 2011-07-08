1e6e44b032e5bcca60e7613d5dc8e20b4da9204e
kriegerj <kriegerj@0d8be23c-4016-4ca5-a69c-3788035ecc6c>
2011-07-08 14:45:48 +0000

    wip
    git-svn-id: https://www.dkfz.de/svn/B040/FCSTOOLS/trunk/QuickFit3@777 0d8be23c-4016-4ca5-a69c-3788035ecc6c

--- application/version.h
+++ application/version.h
@@ -16,14 +16,14 @@ namespace AutoVersion{
 	//Standard Version Type
 	static const long MAJOR = 3;
 	static const long MINOR = 0;
-	static const long BUILD = 166;
-	static const long REVISION = 8;
+	static const long BUILD = 167;
+	static const long REVISION = 9;
 	
 	//Miscellaneous Version Types
 	static const long BUILDS_COUNT = 479;
-	#define RC_FILEVERSION 3,0,166,8
-	#define RC_FILEVERSION_STRING "3, 0, 166, 8\0"
-	static const char FULLVERSION_STRING[] = "3.0.166.8";
+	#define RC_FILEVERSION 3,0,167,9
+	#define RC_FILEVERSION_STRING "3, 0, 167, 9\0"
+	static const char FULLVERSION_STRING[] = "3.0.167.9";
 	
 	//These values are to keep track of your versioning state, don't modify them.
 	static const long BUILD_HISTORY = 0;
