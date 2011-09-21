b562ead350a5a6a3b6a8d94efe279bd4944f63ef
buchholj <buchholj@0d8be23c-4016-4ca5-a69c-3788035ecc6c>
2011-09-21 12:24:48 +0000

    added reader for raw RH/MS output  - automatic frame size detection  - but no file integrity checks
    git-svn-id: https://www.dkfz.de/svn/B040/FCSTOOLS/trunk/QuickFit3@1007 0d8be23c-4016-4ca5-a69c-3788035ecc6c

---
+++ plugins/imagingfcs/qfrdrimagereaderrh.cpp
@@ -0,0 +1,102 @@
+#include "qfrdrimagereaderrh.h"
+
+#include <QObject>
+#include <QtGlobal>
+
+QFRDRImageReaderRH::QFRDRImageReaderRH() {
+    width=0;
+    height=0;
+    filename="";
+    frameSize=0;
+}
+
+QFRDRImageReaderRH::~QFRDRImageReaderRH() {
+    close();
+}
+
+QString QFRDRImageReaderRH::filter() const {
+    return QObject::tr("raw RH file (*.rrh *.dat)");
+}
+
+QString QFRDRImageReaderRH::formatName() const {
+    return QObject::tr("RRH");
+}
+
+
+unsigned int QFRDRImageReaderRH::calculateFrameSize() {
+  // lets assume we have correct shapes frames here... with correct CRCs
+  //only accepts 32 bit packed data format
+
+  unsigned char *buffer = new unsigned char [MAX_FRAME_SIZE];
+  QDataStream in(file);
+  unsigned int len = in.readRawData((char*)buffer, MAX_FRAME_SIZE);
+
+  unsigned char *bufferPos=buffer;
+  unsigned int pos=0;
+  unsigned frameSize=0;
+  file->seek(0);
+  uint16_t crc=0xFFFFU;
+  do {
+    crc=crc_ccitt(crc,bufferPos,4);
+    bufferPos+=4;
+    pos+=4;
+    uint16_t crc0=*((uint16_t*)bufferPos);
+    if(crc0==crc)frameSize=pos;
+  }while((pos<len)||(frameSize!=0));
+  file->seek(0);
+  return frameSize;
+}
+
+
+bool QFRDRImageReaderRH::open(QString filename) {
+  bool result=true;
+  file = new QFile(filename);
+  if (!file->open(QIODevice::ReadOnly))
+    return false;
+  frameSize=calculateFrameSize();
+  switch(frameSize) {
+    case  134: width= 32; height= 32; break;
+    case 2058: width=128; height=128; break;
+    default: frameSize=0; break;
+  }
+  if(width==0||height==0) {
+    close();
+    result=false;
+  }
+  return result;
+}
+
+void QFRDRImageReaderRH::close() {
+  file->close();
+}
+
+uint32_t QFRDRImageReaderRH::countFrames() {
+  uint32_t result=0;
+
+  unsigned int size=file->size();
+  if(frameSize!=0) {
+    if((size % frameSize)==0)
+      result=size/frameSize;
+  }
+  return result;
+}
+
+bool QFRDRImageReaderRH::nextFrame() {
+    return not file->atEnd();
+}
+
+uint16_t QFRDRImageReaderRH::frameWidth() {
+    return width;
+}
+
+uint16_t QFRDRImageReaderRH::frameHeight() {
+    return height;
+}
+
+bool QFRDRImageReaderRH::readFrameUINT16(uint16_t* data) {
+  return readFrame_<uint16_t>(data);
+}
+
+bool QFRDRImageReaderRH::readFrameFloat(float* data) {
+  return readFrame_<float>(data);
+}
---
+++ plugins/imagingfcs/qfrdrimagereaderrh.h
@@ -0,0 +1,73 @@
+#ifndef QFRDRIMAGEREADERRH_H
+#define QFRDRIMAGEREADERRH_H
+
+#include "qfrdrimagereader.h"
+#include "../../../LIB/trunk/frame.h"
+#include "../../../LIB/trunk/crc_ccitt.h"
+#include <QFile>
+#include <QDataStream>
+#include <stdint.h>
+
+#define MAX_FRAME_SIZE (2058)
+
+/*! \brief image reader class for Radhard raw images (implements QFRDRImageReader)
+    \ingroup qf3rdrdp_imaging_fcs
+
+    \note <<<THIS CODE IS NOT PLATFORM INDEPENDENT>>>
+    This class is used to read an image series frame by frame. It is used in this manner:
+*/
+class QFRDRImageReaderRH : public QFRDRImageReader {
+    public:
+        QFRDRImageReaderRH();
+        ~QFRDRImageReaderRH();
+        /** \brief open the given image sequence file
+         *  \param filename name of the image sequence file
+         *  \return \c true on success
+         */
+        virtual bool open(QString filename);
+        /** \brief close the currently opened image file */
+        virtual void close();
+        /** \brief return the number of frames in the files.
+         *
+         *  This does not change the state of the class. This method may have a long runtime, if it has to really count the frames!
+         */
+        virtual uint32_t countFrames();
+        /** \brief move on to the next frame in the file. return \c false if no further image exists */
+        virtual bool nextFrame();
+        /** \brief return the width of the frames (valid after open() returned \c true */
+        virtual uint16_t frameWidth();
+        /** \brief return the height of the frames (valid after open() returned \c true */
+        virtual uint16_t frameHeight();
+        /** \brief read a new frame into the given array of floating point numbers */
+        virtual bool readFrameFloat(float* data);
+        /** \brief read a new frame into the given array of integers */
+        virtual bool readFrameUINT16(uint16_t* data);
+        /** \brief return a filter string for the file format */
+        virtual QString filter() const;
+        /** \brief return a name string for the file format */
+        virtual QString formatName() const ;
+    protected:
+        uint16_t width;
+        uint16_t height;
+        QString filename;
+        unsigned int frameSize;
+        QFile *file;
+
+        unsigned int calculateFrameSize();
+
+        template < typename T >
+        bool readFrame_(T* data) {
+          bool result=false;
+          QDataStream in(file);
+          frame<T> *f = new frame<T>(data,width,height);
+          char *buffer = new char [frameSize];
+          unsigned int len = in.readRawData(buffer, frameSize);
+          if(buffer[0]==0xFFU) {
+            f->template load_packed<uint32_t>((uint32_t*)&(buffer[8]));
+            result=true;
+          }
+          return result;
+        }
+};
+
+#endif // QFRDRIMAGEREADERRH_H
