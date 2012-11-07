b1c831dae854e19f1734f671ed48df25161daa5e
kriegerj <kriegerj@0d8be23c-4016-4ca5-a69c-3788035ecc6c>
2012-11-07 19:23:19 +0000

    git-svn-id: https://www.dkfz.de/svn/B040/FCSTOOLS/trunk/QuickFit3@1878 0d8be23c-4016-4ca5-a69c-3788035ecc6c

---
+++ application/dlgrdrsetproperty.cpp
@@ -0,0 +1,107 @@
+#include "dlgrdrsetproperty.h"
+#include "ui_dlgrdrsetproperty.h"
+
+dlgRDRSetProperty::dlgRDRSetProperty(QWidget *parent) :
+    QDialog(parent),
+    ui(new Ui::dlgRDRSetProperty)
+{
+    ui->setupUi(this);
+}
+
+dlgRDRSetProperty::~dlgRDRSetProperty() {
+    QSettings* settings=ProgramOptions::getInstance()->getQSettings();
+    QString prefix="QuickFit/dlgRDRSetProperty/";
+
+    settings->setValue(prefix+"name", ui->edtName->text());
+    settings->setValue(prefix+"value", ui->edtValue->text());
+    settings->setValue(prefix+"type", ui->cmbType->currentIndex());
+    settings->setValue(prefix+"overwrite", ui->chkOverwrite->isChecked());
+    settings->setValue(prefix+"create", ui->chkCreate->isChecked());
+
+    delete ui;
+}
+
+void dlgRDRSetProperty::init(QFProject *project)
+{
+    this->project=project;
+    rdrs.clear();
+    for (int i=0; i<project->getRawDataCount(); i++) {
+        QPointer<QFRawDataRecord> r=project->getRawDataByNum(i);
+        rdrs.append(r);
+        QListWidgetItem* item=new QListWidgetItem(r->getName(), ui->lstRDR);
+        item->setCheckState(Qt::Checked);
+        item->setFlags(Qt::ItemIsSelectable|Qt::ItemIsUserCheckable|Qt::ItemIsEnabled);
+        item->setData(Qt::UserRole, r->getID());
+        item->setIcon(r->getSmallIcon());
+        ui->lstRDR->addItem(item);
+    }
+
+    QSettings* settings=ProgramOptions::getInstance()->getQSettings();
+    QString prefix="QuickFit/dlgRDRSetProperty/";
+
+    ui->edtName->setText(settings->value(prefix+"name", QString("new_property_name")).toString());
+    ui->edtValue->setText(settings->value(prefix+"value", QString("")).toString());
+    ui->cmbType->setCurrentIndex(settings->value(prefix+"type", 0).toInt());
+    ui->chkCreate->setChecked(settings->value(prefix+"create", true).toBool());
+    ui->chkOverwrite->setChecked(settings->value(prefix+"overwrite", true).toBool());
+}
+
+bool dlgRDRSetProperty::doOverwrite() const
+{
+    return ui->chkOverwrite->isChecked();
+}
+
+bool dlgRDRSetProperty::doCreateNew() const
+{
+    return ui->chkCreate->isChecked();
+}
+
+QList<QPointer<QFRawDataRecord> > dlgRDRSetProperty::getSelectedRDRs() const
+{
+    QList<QPointer<QFRawDataRecord> > l;
+    for (int i=0; i<ui->lstRDR->count(); i++)  {
+        if (ui->lstRDR->item(i)->checkState()==Qt::Checked) {
+            l<<rdrs[i];
+        }
+    }
+    return l;
+}
+
+void dlgRDRSetProperty::on_btnSelectNone_clicked()
+{
+    for (int i=0; i<ui->lstRDR->count(); i++)  {
+        ui->lstRDR->item(i)->setCheckState(Qt::Unchecked);
+    }
+}
+
+void dlgRDRSetProperty::on_btnSelectAll_clicked()
+{
+    for (int i=0; i<ui->lstRDR->count(); i++)  {
+        ui->lstRDR->item(i)->setCheckState(Qt::Checked);
+    }
+}
+
+
+QStringList dlgRDRSetProperty::getNewPropNames() const
+{
+    QStringList sl;
+    sl<<ui->edtName->text();
+    return sl;
+}
+
+QList<QVariant> dlgRDRSetProperty::getNewPropValues() const
+{
+    QList<QVariant> l;
+    if (ui->cmbType->currentIndex()==0) {
+        l<<ui->edtValue->text();
+    } else if (ui->cmbType->currentIndex()==1) {
+        l<<QStringToDouble( ui->edtValue->text());
+    } else if (ui->cmbType->currentIndex()==2) {
+        l<<ui->edtValue->text().toInt();
+    } else if (ui->cmbType->currentIndex()==1) {
+        l<<QStringToBool(ui->edtValue->text());
+    } else {
+        l<<QString("");
+    }
+    return l;
+}
---
+++ application/dlgrdrsetproperty.h
@@ -0,0 +1,38 @@
+#ifndef DLGRDRSETPROPERTY_H
+#define DLGRDRSETPROPERTY_H
+
+#include <QDialog>
+#include "qfproject.h"
+#include "qfrawdatarecord.h"
+#include "programoptions.h"
+#include "qftools.h"
+
+namespace Ui {
+class dlgRDRSetProperty;
+}
+
+class dlgRDRSetProperty : public QDialog {
+        Q_OBJECT
+
+    public:
+        explicit dlgRDRSetProperty(QWidget *parent = 0);
+        ~dlgRDRSetProperty();
+
+        void init(QFProject* project);
+        bool doOverwrite() const;
+        bool doCreateNew() const;
+
+        QList<QPointer<QFRawDataRecord> > getSelectedRDRs() const;
+        QStringList getNewPropNames() const;
+        QList<QVariant> getNewPropValues() const;
+
+    protected slots:
+        void on_btnSelectNone_clicked();
+        void on_btnSelectAll_clicked();
+    private:
+        Ui::dlgRDRSetProperty *ui;
+        QFProject* project;
+        QList<QPointer<QFRawDataRecord> > rdrs;
+};
+
+#endif // DLGRDRSETPROPERTY_H
