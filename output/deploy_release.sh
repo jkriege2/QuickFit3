eef251fb28a28b5e073ce9771f5c1545a47d8c49
kriegerj <kriegerj@0d8be23c-4016-4ca5-a69c-3788035ecc6c>
2012-11-19 20:43:10 +0000

    QuickFit3: deploy-script now supports QtScript module for SPIM only!
    git-svn-id: https://www.dkfz.de/svn/B040/FCSTOOLS/trunk/QuickFit3@1904 0d8be23c-4016-4ca5-a69c-3788035ecc6c

--- output/deploy_release.sh
+++ output/deploy_release.sh
@@ -10,6 +10,7 @@ ZIPFILE=quickfit3_${SVNVER}.zip
 ZIPFILESPIM=quickfit3_spimplugins_${SVNVER}.zip
 SPIMPLUGINS=" cam_testcamera stage_pi863 cam_andor spimb040 shutter_servo_arduino filterc_test cam_systemcam filterc_tmcl lights_b040laserbox lights_pccsled"
 REMOVEPLUGINS=" ${SPIMPLUGINS} cam_radhard2 cam_rh2v2 alv_autocorrelator5000 b040_ffmcontrol multicontrol_stage qfe_acquisitiontest scanner2000_nicounter"
+SPIMONLYQTMODULES=" QtScript4.dll QtScriptTools4.dll"
 
 rm -rf deploy
 rm -rf deployspim
@@ -47,6 +48,12 @@ rm ./globalconfig/*
 rmdir ./globalconfig
 rm ./qtplugins/q*d4.dll
 rm ./Qt*d4.dll
+rm
+
+for f in $SPIMONLYQTMODULES
+do
+    rm -rf  "./${f}"
+done
 
 find -name "quickfit3_*.zip" -exec rm -rf {} \;
 find -name "*.sh" -exec rm -rf {} \;
@@ -68,6 +75,10 @@ mkdir ../deployspim/assets/plugins
 mkdir ../deployspim/assets/plugins/help
 
 cp -rf ./globalconfig_templates/* ../deployspim/globalconfig_templates
+for f in $SPIMONLYQTMODULES
+do
+    cp -rf "../${f}" ../deployspim
+done
 
 for f in $SPIMPLUGINS
 do
