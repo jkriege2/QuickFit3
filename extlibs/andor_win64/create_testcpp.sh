ecb72152feecd96a45f9599170992bac2a0b1449
kriegerj <kriegerj@0d8be23c-4016-4ca5-a69c-3788035ecc6c>
2013-11-04 18:02:42 +0000

    git-svn-id: https://www.dkfz.de/svn/B040/FCSTOOLS/trunk/QuickFit3@2866 0d8be23c-4016-4ca5-a69c-3788035ecc6c

--- extlibs/andor_win64/create_testcpp.sh
+++ extlibs/andor_win64/create_testcpp.sh
@@ -45,37 +45,70 @@ EXPORTS' > atmcd32d.def
 dlltool -d atmcd32d.def --dllname atmcd32d.dll --output-lib libandor.a
 
 cp test.cpp test1.cpp
-sed 's/EXPNETTYPE unsigned int WINAPI /    /g' test1.cpp > test1.~cp | cp test1.~cp test1.cpp
-sed 's/[^\,^\(]*\*[^\,^\)]*/NULL/g' test1.cpp > test1.~cp | cp test1.~cp test1.cpp
-sed 's/(void)/()/g' test1.cpp > test1.~cp | cp test1.~cp test1.cpp
-sed 's/unsigned long[^,^)]*/0/g' test1.cpp > test1.~cp | cp test1.~cp test1.cpp
-sed 's/unsigned int[^,^)]*/0/g' test1.cpp > test1.~cp | cp test1.~cp test1.cpp
-sed 's/int[^,^)]*/0/g' test1.cpp > test1.~cp | cp test1.~cp test1.cpp
-sed 's/long[^,^)]*/0/g' test1.cpp > test1.~cp | cp test1.~cp test1.cpp
-sed 's/double[^,^)]*/0.0/g' test1.cpp > test1.~cp | cp test1.~cp test1.cpp
-sed 's/float[^,^)]*/0.0/g' test1.cpp > test1.~cp | cp test1.~cp test1.cpp
-sed 's/at_u32[^,^)]*/0/g' test1.cpp > test1.~cp | cp test1.~cp test1.cpp
-sed 's/BYTE[^,^)^(]*/0/g' test1.cpp > test1.~cp | cp test1.~cp test1.cpp
-sed 's/short[^,^)]*/0/g' test1.cpp > test1.~cp | cp test1.~cp test1.cpp
-sed 's/DWORD[^,^)]*/0/g' test1.cpp > test1.~cp | cp test1.~cp test1.cpp
-sed 's/at_u64[^,^)]*/0/g' test1.cpp > test1.~cp | cp test1.~cp test1.cpp
-sed 's/const//g' test1.cpp > test1.~cp | cp test1.~cp test1.cpp
-sed 's/unsigned char [^,^)]*/0/g' test1.cpp > test1.~cp | cp test1.~cp test1.cpp
-sed 's/HANDLE[^,^)^(]*/NULL/g' test1.cpp > test1.~cp | cp test1.~cp test1.cpp
-sed 's/HWND[^,^)]*/NULL/g' test1.cpp > test1.~cp | cp test1.~cp test1.cpp
-sed 's/AT_DDGLiteChannelId[^,^)]*/AT_DDGLite_ChannelA/g' test1.cpp > test1.~cp | cp test1.~cp test1.cpp
-sed 's/AT_VersionInfoId[^,^)]*/AT_SDKVersion/g' test1.cpp > test1.~cp | cp test1.~cp test1.cpp
-sed 's/INCLUDEMAIN/int main()/g' test1.cpp > test1.~cp | cp test1.~cp test1.cpp
+sed 's/EXPNETTYPE unsigned int WINAPI /    /g' test1.cpp > test1.~cp 
+cp test1.~cp test1.cpp
+sed 's/[^\,^\(]*\*[^\,^\)]*/NULL/g' test1.cpp > test1.~cp 
+ cp test1.~cp test1.cpp
+sed 's/(void)/()/g' test1.cpp > test1.~cp
+cp test1.~cp test1.cpp
+sed 's/unsigned long[^,^)]*/0/g' test1.cpp > test1.~cp
+cp test1.~cp test1.cpp
+sed 's/unsigned int[^,^)]*/0/g' test1.cpp > test1.~cp
+cp test1.~cp test1.cpp
+sed 's/int[^,^)]*/0/g' test1.cpp > test1.~cp
+cp test1.~cp test1.cpp
+sed 's/long[^,^)]*/0/g' test1.cpp > test1.~cp
+cp test1.~cp test1.cpp
+sed 's/double[^,^)]*/0.0/g' test1.cpp > test1.~cp
+cp test1.~cp test1.cpp
+sed 's/float[^,^)]*/0.0/g' test1.cpp > test1.~cp
+cp test1.~cp test1.cpp
+sed 's/at_u32[^,^)]*/0/g' test1.cpp > test1.~cp
+cp test1.~cp test1.cpp
+sed 's/BYTE[^,^)^(]*/0/g' test1.cpp > test1.~cp
+cp test1.~cp test1.cpp
+sed 's/short[^,^)]*/0/g' test1.cpp > test1.~cp
+cp test1.~cp test1.cpp
+sed 's/DWORD[^,^)]*/0/g' test1.cpp > test1.~cp
+cp test1.~cp test1.cpp
+sed 's/at_u64[^,^)]*/0/g' test1.cpp > test1.~cp
+cp test1.~cp test1.cpp
+sed 's/const//g' test1.cpp > test1.~cp
+cp test1.~cp test1.cpp
+sed 's/unsigned char [^,^)]*/0/g' test1.cpp > test1.~cp
+cp test1.~cp test1.cpp
+sed 's/HANDLE[^,^)^(]*/NULL/g' test1.cpp > test1.~cp
+cp test1.~cp test1.cpp
+sed 's/HWND[^,^)]*/NULL/g' test1.cpp > test1.~cp
+cp test1.~cp test1.cpp
+sed 's/AT_DDGLiteChannelId[^,^)]*/AT_DDGLite_ChannelA/g' test1.cpp > test1.~cp
+cp test1.~cp test1.cpp
+sed 's/AT_VersionInfoId[^,^)]*/AT_SDKVersion/g' test1.cpp > test1.~cp
+cp test1.~cp test1.cpp
+sed 's/INCLUDEMAIN/int main()/g' test1.cpp > test1.~cp
+cp test1.~cp test1.cpp
+
+
+
 g++ test1.cpp -o test1.exe -L. -landor 2> err.txt
-sed 's/.*__imp_//g' err.txt > err.~tx | cp err.~tx err.txt
-sed 's/.*exit status.*//g' err.txt > err.~tx | cp err.~tx err.txt
-sed 's/'\''//g' err.txt > err.~tx | cp err.~tx err.txt
-sed 's/.*undefined reference.*//g' err.txt > err.~tx | cp err.~tx err.txt
+cp err.txt  err_out.txt
+sed -n '/.*__imp.*/p' err.txt > err.~tx
+cp err.~tx err.txt
+sed 's/.*__imp_//g' err.txt > err.~tx
+cp err.~tx err.txt
+sed 's/.*exit status.*//g' err.txt > err.~tx
+cp err.~tx err.txt
+sed 's/'\''//g' err.txt > err.~tx
+cp err.~tx err.txt
+sed 's/.*undefined reference.*//g' err.txt > err.~tx
+cp err.~tx err.txt
+sed 's/.*undefined reference.*//g' err.txt > err.~tx
+cp err.~tx err.txt
 
 sed '$r err.txt' <atmcd32d.def >atmcd32d.~def
 cp atmcd32d.~def atmcd32d.def
 dlltool -d atmcd32d.def --dllname atmcd32d.dll --output-lib libandor.a -k
-rm -f test.~cpp test1.cpp err.~tx  err.txt test1.exe atmcd32d.~h atmcd32d.~hh
+#rm -f test.~cpp test1.cpp err.~tx  err.txt test1.exe atmcd32d.~h atmcd32d.~hh
 
 
 
