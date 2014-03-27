3f8192281a46bf5400c7cb9f6e9eabdf3c985e09
kriegerj <kriegerj@0d8be23c-4016-4ca5-a69c-3788035ecc6c>
2014-03-27 16:21:16 +0000

    git-svn-id: https://www.dkfz.de/svn/B040/FCSTOOLS/trunk/QuickFit3@3113 0d8be23c-4016-4ca5-a69c-3788035ecc6c

--- plugins/spimb040/assets/acquisitionScriptExamples/new_acquisition_script.2.js
+++ plugins/spimb040/assets/acquisitionScriptExamples/new_acquisition_script.2.js
@@ -1,18 +1,19 @@
 
 //params=new Array(1.0,2.0,5.0,8.0,10.0,15.0,20.0,25.0);
-params=new Array(20,15,10,5,2,1,0);
+params=new Array(20,10,0);
 delay=2000;
-
-for (var i=0; i<params.length; i++) {
-    tools.logText("setting laser to " + params[i]+"\n");
+for (var j=0; j<2; j++) {
+  for (var i=0; i<params.length; i++) {
+    tools.logText("setting LED to " + params[i]+"\n");
     instrument.setBrightfieldIntensity(0, 0, params[i]);
-    if(params[i]==0){
-      instrument.setShutter("transmission",false);
-    }else{
-      instrument.setShutter("transmission",true);
-    }
     tools.sleepMS(delay);
     tools.logText("running acquisition at " + params[i]+"mA\n");
-    instrument.setAcqDescCurrentSample("LEDIntensity_24V_"+params[i]+"mA");
+    comment="";
+    if(j==1){
+      acquisition.setAcquisitionCamera2SettingN("GATING_RND", true);
+   comment="_RND"
+  }
+    instrument.setAcqDescCurrentSample("LED_24V_"+params[i]+"mA"+comment);
     acquisition.doAcquisition();
+  }
 }
