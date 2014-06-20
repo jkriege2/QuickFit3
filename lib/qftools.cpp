18bf0591715d64c39fed749e54c0493bbe43afe5
kriegerj <kriegerj@0d8be23c-4016-4ca5-a69c-3788035ecc6c>
2014-06-20 15:06:59 +0000

    git-svn-id: https://www.dkfz.de/svn/B040/FCSTOOLS/trunk/QuickFit3@3266 0d8be23c-4016-4ca5-a69c-3788035ecc6c

--- lib/qftools.cpp
+++ lib/qftools.cpp
@@ -842,7 +842,28 @@ QString qchartostr(QChar data) {
 }
 
 
-QColor getCycleColor(int value, unsigned int max, double S, double V) {
+QColor /*
+    Copyright (c) 2008-2014 Jan W. Krieger, German Cancer Research Center
+
+    last modification: $LastChangedDate$  (revision $Rev$)
+
+    This file is part of QuickFit 3 (http://www.dkfz.de/Macromol/quickfit).
+
+    This software is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+getCycleColor(int value, unsigned int max, double S, double V) {
     while (value<0) value=value+max;
     QColor col;
     col.setHsvF(double(value%max)/double(max-1)*0.8, S, V);
