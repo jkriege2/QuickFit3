aeaeefa3f4c38107549e1cfe5f2acf890b889217
kriegerj <kriegerj@0d8be23c-4016-4ca5-a69c-3788035ecc6c>
2014-07-11 14:10:58 +0000

    git-svn-id: https://www.dkfz.de/svn/B040/FCSTOOLS/trunk/QuickFit3@3354 0d8be23c-4016-4ca5-a69c-3788035ecc6c

--- plugins/spimb040/assets/acquisitionScriptExamples/acquisition_emccd_LED_EMGAIN_EXPOSURE.js
+++ plugins/spimb040/assets/acquisitionScriptExamples/acquisition_emccd_LED_EMGAIN_EXPOSURE.js
@@ -2,9 +2,9 @@
 params=new Array(0,1,2,3,4,5,7,10,12,15,17,20,25,30,35,40,45,50,55,60,65,70,75,80,85,90,95,100,110,120,130,14,150,160,170,180,190,200,250,300,400,500,600,700,800,900,1000);
 //exposures=new Array(10000,1000);
 exposure=10000;
-delay=2000;
+delay=20;
 frames=1000;
-emgain=new Array(0,5,10,100);
+emgain=new Array(0,5,10,50,100);
 
 for (var i=0; i<params.length; i++) {
     tools.logText("setting LED to " + params[i]+"\n");
@@ -13,13 +13,14 @@ for (var i=0; i<params.length; i++) {
     tools.logText("running acquisition at " + params[i]+"mA\n");
 
     for (var jj=0; jj<emgain.length; jj++) {
-          tools.logText(i+":"+jj+"/"+emgain.length+"  LED" + params[i] + "mA__emgain" + emgain[jj] + "_exp" + exposure+ "__\n"); 
-          acquisition.setAcquisitionCamera1Setting("frames",  frames);
-          acquisition.setAcquisitionCamera1Setting("emgain", emgain[jj]);
-          acquisition.setAcquisitionCamera1Setting("exposuretime", exposure);//exposures[k]);
-          instrument.setAcqDescCurrentSample("LED" + params[i] + "mA__emgain" + emgain[jj] + "_exp" + exposure+ "__"); //exposures[k] + "__" );
-          acquisition.doAcquisition();
-
+          if (emgain[jj]<=10 || (emgain[jj]<=50 && params[i]<=120) || (emgain[jj]<=100 && params[i]<=60)) {
+            tools.logText(i+":"+jj+"/"+emgain.length+"  LED" + params[i] + "mA__emgain" + emgain[jj] + "_exp" + exposure+ "__\n"); 
+            acquisition.setAcquisitionCamera1Setting("frames",  frames);
+            acquisition.setAcquisitionCamera1Setting("emgain", emgain[jj]);
+            acquisition.setAcquisitionCamera1Setting("exposuretime", exposure);
+            instrument.setAcqDescCurrentSample("LED" + params[i] + "mA__emgain" + emgain[jj] + "_exp" + exposure+ "__"); //exposures[k] + "__" );
+            acquisition.doAcquisition();
+          }  
     }
 }
 
