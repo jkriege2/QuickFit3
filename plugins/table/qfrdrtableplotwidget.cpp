810ba15005c0000ea07792bda7ba136c8985dc0f
Fereydoon Taheri <taheri@stud.uni-heidelberg.de>
2015-08-12 13:01:12 +0200

    Linux bugfix

--- plugins/table/qfrdrtableplotwidget.cpp
+++ plugins/table/qfrdrtableplotwidget.cpp
@@ -19,9 +19,8 @@
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
-
-#include "ui_qfrdrtableplotwidget.h"
 #include "qfrdrtableplotwidget.h"
+#include "ui_qfrdrtableplotwidget.h"
 #include "programoptions.h"
 #include "qfrdrtable.h"
 #include "qftools.h"
